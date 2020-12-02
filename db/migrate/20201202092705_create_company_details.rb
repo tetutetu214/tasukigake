d1bb462b52f4feb39535d36f23fa6c7d159d99ab
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-02 18:33:58 +0900

    companies_details db

---
+++ db/migrate/20201202092705_create_company_details.rb
@@ -0,0 +1,16 @@
+class CreateCompanyDetails < ActiveRecord::Migration[6.0]
+  def change
+    create_table :company_details do |t|
+      t.integer :division, null: false
+      t.integer :representative, null: false
+      t.string :phone_number , null: false
+      t.string :url
+      t.numeric :capital     , null: false
+      t.date :establishment   , null: false
+      t.integer :employee, null: false
+      t.text :description , null: false
+      t.references :company ,foreign_key: true 
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_055040) do
+ActiveRecord::Schema.define(version: 2020_12_02_092705) do
 
   create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "company_name", null: false
@@ -22,6 +22,21 @@ ActiveRecord::Schema.define(version: 2020_12_02_055040) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_companies_on_user_id"
+  end
+
+  create_table "company_details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "division", null: false
+    t.integer "representative", null: false
+    t.string "phone_number", null: false
+    t.string "url"
+    t.decimal "capital", precision: 10, null: false
+    t.date "establishment", null: false
+    t.integer "employee", null: false
+    t.text "description", null: false
+    t.bigint "company_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_company_details_on_company_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -37,4 +52,5 @@ ActiveRecord::Schema.define(version: 2020_12_02_055040) do
   end
 
   add_foreign_key "companies", "users"
+  add_foreign_key "company_details", "companies"
 end
