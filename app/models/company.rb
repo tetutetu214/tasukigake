2151f1c017919e2427f6191f9309f7058f400c6e
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-02 18:40:40 +0900

    detail validation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ApplicationRecord
   belongs_to :user
+  has_one :company_detail 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
@@ -10,4 +11,9 @@ class Company < ApplicationRecord
     validates :prefecture_id
     validates :first_name_kana
   end
+
+  with_options numericality: { other_than: 1 } do
+    validates :prefecture_id
+  end
+  
 end
--- app/models/company_detail.rb
+++ app/models/company_detail.rb
@@ -1,2 +1,14 @@
 class CompanyDetail < ApplicationRecord
+  belongs_to :CompanyDetail
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :division
+    validates :representative
+    validates :phone_number
+    validates  :capital
+    validates  :estblishment
+    validates  :employee
+    validates  :description
+  end
 end
