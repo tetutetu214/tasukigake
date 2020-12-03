829ba92a9e5ea17504ef8bdcf94da3af311d2a7e
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-03 12:10:36 +0900

    orders model

---
+++ db/migrate/20201203030241_create_orders.rb
@@ -0,0 +1,21 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+
+      t.string :order_title , null: false
+      t.integer :prefecture_id, null: false
+      t.string :order_city
+      t.string :order_address
+      t.date :order_delivery_date_first   , null: false
+      t.date :order_delivery_date_date_end   , null: false
+      t.integer :industry_type_id, null: false
+      t.integer :division, null: false
+      t.integer :unit_price, null: false
+      t.numeric :budget, null: false
+      t.integer :payment_id, null: false
+      t.text :order_pr, null: false
+      t.references :user ,foreign_key: true 
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_102335) do
+ActiveRecord::Schema.define(version: 2020_12_03_030241) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -92,6 +92,25 @@ ActiveRecord::Schema.define(version: 2020_12_02_102335) do
     t.index ["company_detail_id"], name: "index_company_types_on_company_detail_id"
   end
 
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "order_title", null: false
+    t.integer "prefecture_id", null: false
+    t.string "order_city"
+    t.string "order_address"
+    t.date "order_delivery_date_first", null: false
+    t.date "order_delivery_date_date_end", null: false
+    t.integer "industry_type_id", null: false
+    t.integer "division", null: false
+    t.integer "unit_price", null: false
+    t.decimal "budget", precision: 10, null: false
+    t.integer "payment_id", null: false
+    t.text "order_pr", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -110,4 +129,5 @@ ActiveRecord::Schema.define(version: 2020_12_02_102335) do
   add_foreign_key "company_details", "companies"
   add_foreign_key "company_statuses", "company_correspondences"
   add_foreign_key "company_types", "company_details"
+  add_foreign_key "orders", "users"
 end
