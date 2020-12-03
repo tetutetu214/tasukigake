9acbce28dfefd2839c055b2426e725a4103ffa8d
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-03 15:21:32 +0900

    company 1page create

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,10 +6,10 @@ class Company < ApplicationRecord
   belongs_to :prefecture
 
   with_options presence: true do
-    validates :companies_name
+    validates :company_name
     validates :postal_code
     validates :prefecture_id
-    validates :first_name_kana
+    
   end
 
   with_options numericality: { other_than: 1 } do
