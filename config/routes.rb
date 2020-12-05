e0b7f5b253ed726888dcf063179330f9fa9c5580
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-05 14:45:39 +0900

    company_type controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'tops#index'
     devise_for :users
       resources :orders, only: [:index]
-        resources :companies, only: [:index,:create]
-          resources :companies_details, only: [:index,:create]
+        resources :companies, only: [:index,:create,:edit,:update]
+          resources :companies_details, only: [:index,:create,:edit,:update]
+           resources :companies_types, only: [:index,:create,:edit,:update]
 
 end
---
+++ spec/requests/company_types_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "CompanyTypes", type: :request do
+
+end
