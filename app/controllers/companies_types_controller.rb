996468464753f147c8975d80fb0aba65c7c07f01
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-05 15:18:36 +0900

    company_type controller edit

--- app/controllers/companies_types_controller.rb
+++ app/controllers/companies_types_controller.rb
@@ -1,15 +1,15 @@
 class CompaniesTypesController < ApplicationController
   def index
-    @company_detail = CompanyDetail.new
+    @company_type = CompanyType.new
   end
 
   def new
   end
 
   def create
-    @company_detail = CompanyDetail.new(company_detail_params)
-    if @company_detail.valid?
-      @company_detail.save
+    @company_type = CompanyType.new(company_type_params)
+    if @company_type.valid?
+      @company_type.save
       redirect_to  companies_types_path
     else
       render :index
@@ -18,7 +18,7 @@ class CompaniesTypesController < ApplicationController
 
   private
 
-  def company_detail_params
-    params.require(:company_detail).permit(:division, :representative, :phone_number, :url, :capital,:establishment, :employee, :description).merge(company_id: current_user.id)
+  def company_type_params
+    params.require(:company_type).permit(:industry_type_1, :industry_type_2, :industry_type_3).merge(company_detail_id: current_user.id)
   end
 end
