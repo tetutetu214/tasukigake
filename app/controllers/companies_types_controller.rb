ead2b4eaf99f1ae8e1344f4bbeaf77042bfa403f
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-07 10:46:27 +0900

    Update companies_types_controller.rb

--- app/controllers/companies_types_controller.rb
+++ app/controllers/companies_types_controller.rb
@@ -23,32 +23,3 @@ class CompaniesTypesController < ApplicationController
   end
 end
 
-
-
-
-・コントローラー側：複数チェックの情報を格納して送付する記述
-
-controller側の記述
-``` 
-def create
- @company_type = CompanyType.new(company_type_params)
- if @company_type.valid?
- @company_type.save
- redirect_to companies_types_path
- else
- render :index
- end
-end
-
-private
-
-def company_type_params
- params.require(:company_type).permit(:industry_type =>[]).
- merge(company_detail_id: current_user.id)
-end
-``` 
-
- ・このように保存されています
-
-
-gyazo.com
