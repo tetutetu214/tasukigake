20c17e8913e03359141681ab8138145394626c3f
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-07 11:00:16 +0900

    prefecture controller

--- app/controllers/companies_correspondences_controller.rb
+++ app/controllers/companies_correspondences_controller.rb
@@ -1,2 +1,26 @@
 class CompaniesCorrespondencesController < ApplicationController
+
+  def index
+    @company_correspondence = CompanyCorrespondence.new
+  end
+
+  def new
+  end
+
+  def create
+    @company_correspondence = CompanyCorrespondence.new(company_correspondence_params)
+    if @company_type.valid?
+      @company_type.save
+      redirect_to  companies_types_path
+    else
+      render :index
+    end 
+  end
+
+  private
+
+  def company_correspondence_params
+    params.require(:company_correspondence).permit(:prefecture_type =>[]).merge(company_type_id: current_user.id)
+  end
 end
+
