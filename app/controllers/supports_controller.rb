c4349510aeb5bfe50c5253e8d4ebed1fe0b8c068
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-08 14:58:30 +0900

    support v

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,23 +1,4 @@
 class SupportsController < ApplicationController
-  def new
-    @order = Order.new
+  def index
   end
-
-  def create
-    @order = Order.new(order_params)
-    if @order.valid?
-      @order.save
-      redirect_to orders_path
-    else
-      render :index
-    end
-  end
-
-  private
-
-  def order_params
-    params.require(:order).permit(:order_title, :prefecture_id, :order_city, :order_address, :order_delivery_date_first, :order_delivery_date_end, :division, :unit_price, :budget, :payment_id, :order_pr, industry_type: []).merge(user_id: current_user.id)
-  end
-
-end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'tops#index'
     devise_for :users
       resources :orders, only: [:index,:new,:create,:edit,:update]
-        resources :supports, only: [:index,:new,:create,:edit,:update]
+        resources :supports, only: [:index]
           resources :companies, only: [:index,:create,:edit,:update]
             resources :companies_details, only: [:index,:create,:edit,:update]
               resources :companies_types, only: [:index,:create,:edit,:update]
