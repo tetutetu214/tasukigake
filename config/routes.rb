55c805107faee74a0656e01ba2e74dd78a36ad18
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-08 14:45:34 +0900

    support route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   root 'tops#index'
     devise_for :users
       resources :orders, only: [:index,:new,:create,:edit,:update]
-        resources :companies, only: [:index,:create,:edit,:update]
-          resources :companies_details, only: [:index,:create,:edit,:update]
-           resources :companies_types, only: [:index,:create,:edit,:update]
-            resources :companies_correspondences, only: [:index,:create,:edit,:update]
-              resources :companies_statuses, only: [:index,:create,:edit,:update]
+        resources :supports, only: [:index,:new,:create,:edit,:update]
+          resources :companies, only: [:index,:create,:edit,:update]
+            resources :companies_details, only: [:index,:create,:edit,:update]
+              resources :companies_types, only: [:index,:create,:edit,:update]
+                resources :companies_correspondences, only: [:index,:create,:edit,:update]
+                  resources :companies_statuses, only: [:index,:create,:edit,:update]
 
 end
