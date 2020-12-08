b2659ebd2a07f6eb7959004e00ed5b99b9c7ad44
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-08 21:31:22 +0900

    support controller

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -8,17 +8,18 @@ class SupportsController < ApplicationController
 
   def create
     @support = Support.new(support_params)
+    binding.pry
     if @support.valid?
       @support.save
       redirect_to support_path
     else
-      render :index
+      render :new
     end
   end
 
   private
 
   def support_params
-    params.require(:support).permit(:support_title, :prefecture_id, :support_city, :support_address, :support_delivery_date_first, :support_delivery_date_end, :support_person_id, :termination, :feature, :budget, :support_pr, industry_type: []).merge(user_id: current_user.id)
+    params.require(:support).permit(:support_title,:support_delivery_date_first, :support_delivery_date_end, :support_person_id, :termination, :support_pr, prefecture_type: [],industry_type: [],feature: []).merge(user_id: current_user.id)
   end
 end
--- db/migrate/20201208032943_create_supports.rb
+++ db/migrate/20201208032943_create_supports.rb
@@ -3,16 +3,13 @@ class CreateSupports < ActiveRecord::Migration[6.0]
     create_table :supports do |t|
 
       t.string :support_title  , null: false
-      t.integer :prefecture_id, null: false
-      t.string :support_city
-      t.string :support_address
+      t.text :prefecture_type, null: false
       t.date :support_delivery_date_first   , null: false
       t.date :support_delivery_date_end   , null: false
       t.text :industry_type, null: false
       t.integer :support_person_id, null: false
       t.date :termination, null: false
-      t.integer :feature, null: false
-      t.numeric :budget, null: false
+      t.text :feature, null: false
       t.text :support_pr, null: false
       t.references :user ,foreign_key: true 
       t.timestamps
