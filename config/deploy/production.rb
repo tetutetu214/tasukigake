f69077a9e5809024baf1f5b810f3a4176dec2cc7
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-10 14:38:53 +0900

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '175.41.234.188 ', user: 'ec2-user', roles: %w{app db web}
+server '175.41.234.188', user: 'ec2-user', roles: %w{app db web}
