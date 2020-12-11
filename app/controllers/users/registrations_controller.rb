663c9fd6dbc2e910b55eebe0f54b75ac61896228
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-11 15:43:41 +0900

    user controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,9 +3,23 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+
+
  def new
   @user = User.new
  end
+
+ def create
+  @user = User.new(sign_up_params)
+   unless @user.valid?
+     render :new and return
+   end
+  session["devise.regist_data"] = {user: @user.attributes}
+  session["devise.regist_data"][:user]["password"] = params[:user][:password]
+  @companies = @user.build_companies
+  render :new_companies
+end
+
   # GET /resource/sign_up
   # def new
   #   super
