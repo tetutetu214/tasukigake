d22ad53f5a0eaaeffa1ed36d11fbb4c1eb5d001d
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-12 19:11:38 +0900

    routing create

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,20 @@ Rails.application.routes.draw do
       post 'companies', to: 'users/registrations#create_companies'
        get 'companies_details', to: 'users/registrations#new_companies_details'
        post 'companies_details', to: 'users/registrations#create_companies_details'
+        get 'companies_types', to: 'users/registrations#new_companies_types'
+        post 'companies_types', to: 'users/registrations#create_companies_types'
+         get 'companies_correspondences', to: 'users/registrations#new_companies_correspondences'
+         post 'companies_correspondences', to: 'users/registrations#create_companies_correspondences'
+          get 'companies_statuses', to: 'users/registrations#new_companies_statuses'
+          post 'companies_statuses', to: 'users/registrations#create_companies_statuses'
     end
       resources :orders, only: [:index,:new,:create,:edit,:update]
         resources :supports, only: [:index, :new, :create,:edit,:update,:show]
           resources :companies, only: [:index,:edit,:update]
             resources :companies_details, only: [:index,:edit,:update]
               resources :companies_types, only: [:index,:edit,:update]
-                resources :companies_correspondences, only: [:index,:create,:edit,:update]
-                  resources :companies_statuses, only: [:index,:create,:edit,:update]
-                    resources :project_cases, only: [:index,:new,:create,:edit,:update,:show]
+                resources :companies_correspondences, only: [:index,:edit,:update]
+                  resources :companies_statuses, only: [:index,:edit,:update]
+                    resources :project_cases, only: [:index,:new,:edit,:update]
 
 end
