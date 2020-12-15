28fa2a5860d13f3d2bb7a46a2983f6c98359e536
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-12-15 18:32:46 +0900

    edit page create

--- app/controllers/companies_details_controller.rb
+++ app/controllers/companies_details_controller.rb
@@ -1,5 +1,15 @@
 class CompaniesDetailsController < ApplicationController
-  def index
-    @company_detail = CompanyDetail.new
+  def edit
+    @company_detail = CompanyDetail.find(params[:id])
+  end
+
+  def update
+    @company_detail = Company.find(params[:id])
+    if @company_detail.valid?
+      @company_detail.save
+      redirect_to companies_details_path
+    else
+      render :index
+    end
   end
 end
